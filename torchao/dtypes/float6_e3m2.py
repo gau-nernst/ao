bb836c6bfe5e88608f6a46804a30847e42a2939f
Thien Tran <gau.nernst@yahoo.com.sg>
2024-05-27 01:46:23 +0000

    improve prototype kernel

--- torchao/dtypes/float6_e3m2.py
+++ torchao/dtypes/float6_e3m2.py
@@ -178,104 +178,168 @@ def from_float6_e3m2(tensor: Tensor, no_bit_packing: bool = False, dtype: torch.
     return torch.stack([val0, val1, val2, val3], dim=-1).flatten(-2)
 
 
-if has_triton():
-    @triton.jit
-    def float16_float6_e3m2_matmul(
-        # Pointers to matrices
-        a_ptr,
-        b_ptr,
-        b_2bit_ptr,
-        b_4bit_ptr,
-        c_ptr,
-        s1_ptr,
-        # Matrix dimensions
-        M,
-        N,
-        K,
-        # The stride variables represent how much to increase the ptr by when moving by 1
-        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
-        # by to get the element one row down (A has M rows).
-        stride_am,
-        stride_ak,
-        stride_bk,
-        stride_bn,
-        # stride_cm,
-        # stride_cn,
-        # stride_s1m,
-        # stride_s1n,
-        # Meta-parameters
-        BLOCK_M: tl.constexpr,
-        BLOCK_N: tl.constexpr,
-        BLOCK_K: tl.constexpr,
-        GROUP_M: tl.constexpr,
-        ACC_TYPE: tl.constexpr = tl.float32,
-    ):
-        # based on triton.ops.matmul
-        pid = tl.program_id(0)
-        grid_m = (M + BLOCK_M - 1) // BLOCK_M
-        grid_n = (N + BLOCK_N - 1) // BLOCK_N
+def get_cuda_autotune_config():
+    return [
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256, 'BLOCK_K': 64, 'GROUP_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 256, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 128, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 32, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=5,
+                      num_warps=2),
+        triton.Config({'BLOCK_M': 32, 'BLOCK_N': 64, 'BLOCK_K': 32, 'GROUP_M': 8}, num_stages=5,
+                      num_warps=2),
+        # Good config for fp8 inputs.
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256, 'BLOCK_K': 128, 'GROUP_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 128, 'BLOCK_K': 128, 'GROUP_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 64, 'BLOCK_K': 128, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 256, 'BLOCK_K': 128, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128, 'BLOCK_K': 128, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64, 'BLOCK_K': 64, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 128, 'BLOCK_K': 64, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32, 'BLOCK_K': 64, 'GROUP_M': 8}, num_stages=4,
+                      num_warps=4)
+    ]
 
-        # re-order program ID for better L2 performance
-        width = GROUP_M * grid_n
-        group_id = pid // width
-        group_size = min(grid_m - group_id * GROUP_M, GROUP_M)
-        pid_m = group_id * GROUP_M + (pid % group_size)
-        pid_n = (pid % width) // (group_size)
 
-        rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
-        rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
-        ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M)
-        rk = tl.arange(0, BLOCK_K)
-        A = a_ptr + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
+@triton.autotune(
+    configs=get_cuda_autotune_config(),
+    key=['M', 'N', 'K'],
+)
+@triton.jit
+def float16_float6_e3m2_matmul_kernel(
+    a_ptr,
+    b_2bit_ptr,
+    b_4bit_ptr,
+    c_ptr,
+    s1_ptr,
+    # Matrix dimensions
+    M,
+    N,
+    K,
+    stride_am,
+    stride_ak,
+    # Meta-parameters
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,
+    GROUP_M: tl.constexpr,
+    ACC_TYPE: tl.constexpr = tl.float32,
+    IS_BFLOAT16: tl.constexpr = False,
+):
+    # based on triton.ops.matmul
+    pid = tl.program_id(0)
+    grid_m = (M + BLOCK_M - 1) // BLOCK_M
+    grid_n = (N + BLOCK_N - 1) // BLOCK_N
 
-        # TODO: load b_2bit and b_4bit, reconstruct b
-        # 2bit: load as uint8, 4 values in 1 uint8 -> divide N by 4
-        # 4bit: load as uint8, 2 values in 1 uint8 -> divide N by 2
-        # what if b is transposed? divide K instead
-        rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N)
-        B = b_ptr + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
+    # re-order program ID for better L2 performance
+    width = GROUP_M * grid_n
+    group_id = pid // width
+    group_size = min(grid_m - group_id * GROUP_M, GROUP_M)
+    pid_m = group_id * GROUP_M + (pid % group_size)
+    pid_n = (pid % width) // (group_size)
 
-        B_2bit = b_2bit_ptr + rk[:, None] * (N // 4) + (pid_n * (BLOCK_N // 4)) + tl.arange(0, BLOCK_N // 4)
-        B_4bit = b_4bit_ptr + rk[:, None] * (N // 2) + (pid_n * (BLOCK_N // 2)) + tl.arange(0, BLOCK_N // 2)
+    rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+    ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M)
+    rk = tl.arange(0, BLOCK_K)
+    A = a_ptr + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
 
-        acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=ACC_TYPE)
-        for k in range(K, 0, -BLOCK_K):
-            a = tl.load(A)
-            # b = tl.load(B)
+    tl.static_assert(not IS_BFLOAT16, "bfloat16 is not implemented yet.")
 
-            b_2bit = tl.load(B_2bit)
-            b_4bit = tl.load(B_4bit)
+    # TODO: load b_2bit and b_4bit, reconstruct b
+    # 2bit: load as uint8, 4 values in 1 uint8 -> divide N by 4
+    # 4bit: load as uint8, 2 values in 1 uint8 -> divide N by 2
+    # what if b is transposed? divide K instead
+    # rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N)
+    # B = b_ptr + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
 
-            # unpack 4 2-bit into 4 16-bit
-            b_2bit = b_2bit.to(tl.uint64, bitcast=True)
-            b_2bit = ((b_2bit & 0xc0) << 56) | ((b_2bit & 0x30) << 42) | ((b_2bit & 0x0c) << 28) | ((b_2bit & 0x03) << 14)
-            b_2bit = b_2bit.to(tl.uint16, bitcast=True)
+    B_2bit = b_2bit_ptr + rk[:, None] * (N // 4) + (pid_n * (BLOCK_N // 4)) + tl.arange(0, BLOCK_N // 4)
+    B_4bit = b_4bit_ptr + rk[:, None] * (N // 2) + (pid_n * (BLOCK_N // 2)) + tl.arange(0, BLOCK_N // 2)
 
-            # unpack 2 4-bit into 2 16-bit
-            # TODO: adjust bit shift
-            b_4bit = b_4bit.to(tl.uint32, bitcast=True)
-            b_4bit = ((b_4bit & 0xf0) << 8) | ((b_4bit & 0x0f) << 4)
-            b_4bit = b_4bit.to(tl.uint16, bitcast=True)
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=ACC_TYPE)
+    for k in range(K, 0, -BLOCK_K):
+        a = tl.load(A)
 
-            acc += tl.dot(a, b)
-            A += BLOCK_K * stride_ak
-            # B += BLOCK_K * stride_bk
+        b_2bit = tl.load(B_2bit)
+        b_4bit = tl.load(B_4bit)
 
-            B_2bit += BLOCK_K * (N // 4)
-            B_4bit += BLOCK_K * (N // 2)
+        # unpack 4 2-bit into 4 16-bit
+        b_2bit = b_2bit.to(tl.uint16)
+        b_2bit_0 = (b_2bit & 0xc0) << 8
+        b_2bit_1 = (b_2bit & 0x30) << 10
+        b_2bit_2 = (b_2bit & 0x0c) << 12
+        b_2bit_3 = (b_2bit & 0x03) << 14
+        b_2bit = tl.join(tl.join(b_2bit_0, b_2bit_2), tl.join(b_2bit_1, b_2bit_3))
 
-        # rematerialize rm and rn to save registers
-        rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
-        rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
-        idx_m = rm[:, None]
-        idx_n = rn[None, :]
-        mask = (idx_m < M) & (idx_n < N)
+        # unpack 2 4-bit into 2 16-bit
+        b_4bit = b_4bit.to(tl.uint16)
+        b_4bit_0 = (b_4bit & 0xf0) << 4
+        b_4bit_1 = (b_4bit & 0x0f) << 8
+        b_4bit = tl.join(b_4bit_0, b_4bit_1)
 
-        # inductor generates a suffix
-        xindex = idx_n + (N * idx_m)
-        tmp0 = tl.load(
-            s1_ptr + (tl.broadcast_to(idx_m, mask.shape)),
-            mask,
-            eviction_policy="evict_last",
-        )
-        tl.store(c_ptr + (tl.broadcast_to(xindex, mask.shape)), acc * tmp0, mask)
+        #        sign bit     |   first mantissa bit   | 2 mantissa bits and 2 exponent bits
+        b = (b_2bit & 0x8000) | ((b_2bit & 4000) >> 2) | b_4bit
+        b = b.to(tl.float16, bitcast=True)
+        b *= 2 ** (15 - 3)  # exponent bias correction
+        # TODO: merge scale here?
+
+        acc += tl.dot(a, b)
+        A += BLOCK_K * stride_ak
+
+        B_2bit += BLOCK_K * (N // 4)
+        B_4bit += BLOCK_K * (N // 2)
+
+    # rematerialize rm and rn to save registers
+    rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+    idx_m = rm[:, None]
+    idx_n = rn[None, :]
+    mask = (idx_m < M) & (idx_n < N)
+
+    # inductor generates a suffix
+    xindex = idx_n + (N * idx_m)
+    tmp0 = tl.load(
+        s1_ptr + (tl.broadcast_to(idx_m, mask.shape)),
+        mask,
+        eviction_policy="evict_last",
+    )
+    tl.store(c_ptr + (tl.broadcast_to(xindex, mask.shape)), acc * tmp0, mask)
+
+
+def float16_float6_e3m2_matmul(A: torch.Tensor, B_2bit: torch.Tensor, B_4bit: torch.Tensor) -> torch.Tensor:
+    M, K = A.shape
+    N = B_2bit.shape[1] * 4
+
+    assert A.dtype is torch.float16
+    assert M % 64 == 0
+    assert N % 64 == 0
+
+    scales = torch.ones(M, device=A.device, dtype=A.dtype)
+
+    C = torch.empty(M, N, dtype=A.dtype)
+    grid = lambda META: (triton.cdiv(M, META['BLOCK_M']) * triton.cdiv(N, META['BLOCK_N']),)
+    float16_float6_e3m2_matmul_kernel[grid](A, B_2bit, B_4bit, C, scales, M, N, K, A.stride(0), A.stride(1))
+    return C
+
+
+if __name__ == "__main__":
+    M, N, K = 1024, 1024, 1024
+    A = torch.randn(M, K, device="cuda", dtype=torch.float16)
+    B_2bit = torch.randint(256, size=(K, N // 4), device="cuda", dtype=torch.uint8)
+    B_4bit = torch.randint(256, size=(K, N // 2), device="cuda", dtype=torch.uint8)
+    float16_float6_e3m2_matmul(A, B_2bit, B_4bit)
